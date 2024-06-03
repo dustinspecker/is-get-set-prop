1e03fe957fe92cb63d1c4f104a515b35426972b6
Dustin Specker <DustinSpecker@DustinSpecker.com>
2024-06-02 19:38:22 -0500

    chore(package.json): remove gulp

--- test/test.js
+++ test/test.js
@@ -1,36 +1,31 @@
-/* global describe, it */
 'use strict'
-const chai = require('chai')
+const test = require('ava')
 const isGetSetProp = require('../src/')
 
-const expect = chai.expect
+test('should throw error when type or property is not a string', t => {
+  const typeTest = () => isGetSetProp(1, 'prop')
+  const propTest = () => isGetSetProp('type', 1)
 
-describe('is-get-set-prop', () => {
-  it('should throw error when type or property is not a string', () => {
-    const typeTest = () => isGetSetProp(1, 'prop')
-    const propTest = () => isGetSetProp('type', 1)
+  t.throws(typeTest, TypeError, /t.truthyed a string/)
+  t.throws(propTest, TypeError, /t.truthyed a string/)
+})
 
-    expect(typeTest).to.throw(TypeError, /Expected a string/)
-    expect(propTest).to.throw(TypeError, /Expected a string/)
-  })
+test('should return false if not a js type', t => {
+  t.falsy(isGetSetProp('dog', 'bark'))
+  t.falsy(isGetSetProp('gulp', 'task'))
+})
 
-  it('should return false if not a js type', () => {
-    expect(isGetSetProp('dog', 'bark')).to.eql(false)
-    expect(isGetSetProp('gulp', 'task')).to.eql(false)
-  })
+test('should return false if property is not a getter/setter', t => {
+  t.falsy(isGetSetProp('Array', 'push'))
+  t.falsy(isGetSetProp('Error', 'ignore'))
+})
 
-  it('should return false if property is not a getter/setter', () => {
-    expect(isGetSetProp('Array', 'push')).to.eql(false)
-    expect(isGetSetProp('Error', 'ignore')).to.eql(false)
-  })
+test('shoud return true if property is a getter/setter', t => {
+  t.truthy(isGetSetProp('Array', 'length'))
+  t.truthy(isGetSetProp('Error', 'stack'))
+})
 
-  it('shoud return true if property is a getter/setter', () => {
-    expect(isGetSetProp('Array', 'length')).to.eql(true)
-    expect(isGetSetProp('Error', 'stack')).to.eql(true)
-  })
-
-  it('should be case insensitive for types', () => {
-    expect(isGetSetProp('array', 'length')).to.eql(true)
-    expect(isGetSetProp('ARRAY', 'length')).to.eql(true)
-  })
+test('should be case insenstestive for types', t => {
+  t.truthy(isGetSetProp('array', 'length'))
+  t.truthy(isGetSetProp('ARRAY', 'length'))
 })
